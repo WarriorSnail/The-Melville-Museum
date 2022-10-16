b8ce8cef58aef5c778c2f10a009ddc4ebea64f22
Mikhail Maksimov <110354654+WarriorSnail@users.noreply.github.com>
2022-10-16 20:53:57 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
+const name = document.getElementById('name')//makes name variable
+const email = document.getElementById('email')//makes email variable
+const date = document.getElementById('date')//makes date variable
 const age = document.getElementById('age')//makes age variable
-const form = document.getElementById('form')// makes form variable
-const errorElement = document.getElementById('error')
 const nage = Number(age)// converts age to number
 
-form.addEventListener('submit', (e) => { //this code prevents (tries to) form from submitting with an empty age field or with an age under 18
+FormData.addEventListener('submit', e => {//when submit button is clicked, prevents empty fields from submitting and calls validateinputs method
+  e.preventDefault();
 
-  let messages = []
-  if (age.value === '' || age.value == null) {
-    messages.push("Age is required")
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputcontrol.querySelector('.error');
+
+  errorDisplay.innertext = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+  const nameValue = name.value.trim();
+  const emailValue = email.value.trim();
+  const dateValue = date.value.trim();
+  const nageValue = nage.valueOf.trim();
+
+  if (nameValue === '') {
+    setError(name, 'Name is required');
+  } else {
+    setSuccess(name);
   }
 
-  if (messages.length > 0) {
-    e.preventDefault()
-    errorElement.innerText = messages.join(', ')
-  }
+  if(emailValue === '') {
+    setError(email, 'Email is required');
+} else if (!isValidEmail(emailValue)) {
+    setError(email, 'Provide a valid email address');
+} else {
+    setSuccess(email);
+}
 
-  if (nage.valueOf < 18) {
-    messages.push("You must be 18 or over")
-  }
-  
-})
+if (dateValue === '') {
+  setError(date, 'Date is required');
+} else {
+  setSuccess(date);
+}
+
+};
